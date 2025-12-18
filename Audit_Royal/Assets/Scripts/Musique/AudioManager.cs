02609ceca4b9ecbc40634ddfd048412b6da96988
Cl1ient <teekzie68@gmail.com>
2025-12-18 12:40:02 +0100

    add background musique

---
+++ Audit_Royal/Assets/Scripts/Musique/AudioManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("Configuration Audio")]
+    public AudioSource musiqueSource; 
+    public AudioClip musiqueDeFond;  
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        if (musiqueSource != null && musiqueDeFond != null)
+        {
+            musiqueSource.clip = musiqueDeFond;
+            musiqueSource.loop = true; 
+            musiqueSource.Play();
+        }
+    }
+}
